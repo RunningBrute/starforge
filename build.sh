e4deef20bd59ec5d692b975c8183f4378cd88fb2
RunningBrute <piotrekbas8@gmail.com>
2025-12-01 21:36:27 +0100

    update building script

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 echo "Building Star Forge..."
-clang src/main.c src/starforge_engine.c -Iinclude -o bin/starforge
+clang src/main.c \
+      src/starforge_world.c \
+      src/starforge_particlesystem.c \
+      src/starforge_engine.c \
+      -Iinclude -o bin/starforge
 
 echo "Building tests..."
 clang tests/test_main.c tests/test_engine.c tests/unity/unity.c \
-     src/starforge_engine.c -Iinclude -Itests/unity -o bin/tests_run
+     src/starforge_engine.c \
+     -Iinclude -Itests/unity -o bin/tests_run
 
 echo "Running tests..."
 ./bin/tests_run
