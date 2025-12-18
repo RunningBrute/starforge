39dc9fe0e81c27731dc08377d00386090d2263a4
RunningBrute <piotrekbas8@gmail.com>
2025-12-19 00:20:09 +0100

    add implementation of all getters for aos partition pool

--- src/starforge_particle_pool_aos.c
+++ src/starforge_particle_pool_aos.c
@@ -10,6 +10,51 @@ StarforgeParticlePool* starforge_particle_pool_aos_create(int max_particles)
     return pool;
 }
 
+float get_x(StarforgeParticlePool* pool, int index)
+{
+    return pool->pool[index].x;
+}
+
+float get_y(StarforgeParticlePool* pool, int index)
+{
+    return pool->pool[index].y;
+}
+
+float get_vx(StarforgeParticlePool* pool, int index)
+{
+    return pool->pool[index].vx;
+}
+
+float get_vy(StarforgeParticlePool* pool, int index)
+{
+    return pool->pool[index].vy;
+}
+
+float get_life(StarforgeParticlePool* pool, int index)
+{
+    return pool->pool[index].life;
+}
+
+float get_max_life(StarforgeParticlePool* pool, int index)
+{
+    return pool->pool[index].max_life;
+}
+
+float get_size(StarforgeParticlePool* pool, int index)
+{
+    return pool->pool[index].size;
+}
+
+StarforgeParticleType get_type(StarforgeParticlePool* pool, int index)
+{
+    return pool->pool[index].type;
+}
+
+int get_alive(StarforgeParticlePool* pool, int index)
+{
+    return pool->pool[index].alive;
+}
+
 void starforge_particle_pool_aos_destroy(StarforgeParticlePool* pool)
 {
     free(pool->pool);
